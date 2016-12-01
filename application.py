c8821a123a11a4a7dfd77f4fe26af6c6cc0513de
Michael A Craig <michael.a.craig@warbyparker.com>
2016-11-30 20:03:29 -0500

    Add templates

--- application.py
+++ application.py
@@ -1,19 +1,11 @@
 """ Welp. Here you go. """
 from flask import Flask
 
-def say_hello(username="world"):
-    return '<p>Hello %s!</p>\n' % username
-
-header_text = """
-<html><head><title>Test</title></head><body>"""
-footer_text = '</body></html>'
-
-# EB looks for an 'application' callable by default
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text + \
-                         say_hello() + footer_text))
+@application.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
